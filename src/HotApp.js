f1085c17039edcc5f4dc0d2129e8dd1edf346778
wb-mrf634120 <wb-mrf634120@koubei.com>
2020-01-03 18:46:59 +0800

    add laoding -g

--- src/HotApp.js
+++ src/HotApp.js
@@ -1,12 +1,13 @@
 import React, { PureComponent } from 'react';
 import { Provider } from 'react-redux';
 // import { hot } from 'react-hot-loader';
-import PageRouter from './router/pageRouter.js';
+import PageRouter from './router/pageRouter';
+import SpinWrapper from '@/containers/SpinWrapper';
 
 class HotApp extends PureComponent {
     render() {
         return (
-            <PageRouter />
+            <SpinWrapper pageRouter={PageRouter} />
         )
     }
 }
---
+++ src/containers/SpinWrapper.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent } from 'react';
+import { Spin } from 'antd';
+import '@/style/spin.less';
+
+class SpinWrapper extends PureComponent {
+    render() {
+        const PageRouter = this.props.pageRouter;
+        return (
+            <Spin
+                spinning={false}
+                wrapperClassName='spin-loading'
+                indicator={
+                    <div className="loadEffect">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                }
+            >
+                <PageRouter />
+            </Spin>
+        )
+    }
+}
+export default SpinWrapper;
